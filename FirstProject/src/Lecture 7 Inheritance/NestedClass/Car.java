b4caca984edcf1e4846d9d0b2727c99a8e60ce7d
Divyanshu Ranjan <123241944+divyanshu-17@users.noreply.github.com>
2026-07-06 14:19:50 +0530

    Add files via upload

---
+++ FirstProject/src/Lecture 7 Inheritance/NestedClass/Car.java
@@ -0,0 +1,19 @@
+package NestedClass;
+
+public class Car {
+    private int noOfDoors;
+
+    public void repair(){
+        Tyre t= new Tyre();
+    }
+    protected class Tyre{
+
+        private double width;
+        private double pressure;
+        private String material;
+
+        public void inflate(){
+            noOfDoors=4;
+        }
+    }
+}

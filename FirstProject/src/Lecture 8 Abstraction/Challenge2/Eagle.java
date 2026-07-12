e32ffcc93b9a12ac75948fe484f9fc9a50cc938a
Divyanshu Ranjan <123241944+divyanshu-17@users.noreply.github.com>
2026-07-12 12:00:04 +0530

    Add files via upload

---
+++ FirstProject/src/Lecture 8 Abstraction/Challenge2/Eagle.java
@@ -0,0 +1,8 @@
+package in.Abstra.Challenge2;
+
+public class Eagle extends Bird{
+    @Override
+    public void fly(){
+        System.out.println("Flying.......");
+    }
+}
---
+++ FirstProject/src/Lecture 8 Abstraction/Challenge2/TestFlight.java
@@ -0,0 +1,8 @@
+package in.Abstra.Challenge2;
+
+public class TestFlight {
+    public static void main(String[] args) {
+        Eagle cheel= new Eagle();
+        cheel.fly();
+    }
+}

f46787171f8a01a16f3e3df8a03df918cc6bfca2
Divyanshu Ranjan <123241944+divyanshu-17@users.noreply.github.com>
2026-07-13 05:51:34 +0530

    Add files via upload

---
+++ FirstProject/src/Lecture 9 Polymorphism/AnotherEx/Child1.java
@@ -0,0 +1,10 @@
+package Refrence.AnotherEx;
+
+class Child1 implements ParentP{
+    private String bruh;
+
+    @Override
+    public void go(){
+        System.out.println("c1 walking");
+    }
+}
---
+++ FirstProject/src/Lecture 9 Polymorphism/AnotherEx/Child2.java
@@ -0,0 +1,10 @@
+package Refrence.AnotherEx;
+
+public class Child2 implements ParentP{
+    private String bro;
+
+    @Override
+    public void go(){
+        System.out.println("c2 walking");
+    }
+}

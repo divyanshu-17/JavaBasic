9cd27a33d0499ea6c98f928d42651282fffb1119
Divyanshu Ranjan <123241944+divyanshu-17@users.noreply.github.com>
2026-07-13 05:45:55 +0530

    Add files via upload

---
+++ FirstProject/src/Lecture 9 Polymorphism/Refrence/Relation/VehicleP.java
@@ -0,0 +1,14 @@
+package Refrence.Relation;
+
+public class VehicleP {
+    private String whichVeh;
+
+    public VehicleP(String whichVeh) {
+        this.whichVeh = whichVeh;
+    }
+
+    public void start(){
+        System.out.println("Vehicle going....");
+    }
+
+}

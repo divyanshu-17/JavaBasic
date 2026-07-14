2355cc6799e7d0ec1010128121fb4a324ecf70a1
Divyanshu Ranjan <123241944+divyanshu-17@users.noreply.github.com>
2026-07-14 08:06:14 +0530

    Add files via upload

---
+++ FirstProject/src/Lecture 9 Polymorphism/Final/FinalCar.java
@@ -0,0 +1,9 @@
+package Final;
+
+public class FinalCar extends FinalVehicle{
+
+//    @Override Now we can't change or redefine due to method final
+//    public void start(){
+//        System.out.println("Car start...");
+//    }
+}

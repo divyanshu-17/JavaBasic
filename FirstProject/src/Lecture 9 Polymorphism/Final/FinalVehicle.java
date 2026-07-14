d751335a74ae56bed5555c415864c923258cb0be
Divyanshu Ranjan <123241944+divyanshu-17@users.noreply.github.com>
2026-07-14 08:05:46 +0530

    Add files via upload

---
+++ FirstProject/src/Lecture 9 Polymorphism/Final/FinalVehicle.java
@@ -0,0 +1,13 @@
+package Final;
+//public class FinalVehicle extends FinalMotorVehicle
+
+public class FinalVehicle{
+    public final void start(){
+        System.out.println("Veh start...");
+    }
+
+//    @Override
+//    public void motorRunning() {
+//        System.out.println("Vrooooommm....");
+//    }
+}

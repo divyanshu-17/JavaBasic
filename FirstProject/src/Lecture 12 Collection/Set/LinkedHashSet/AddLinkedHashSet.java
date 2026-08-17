be6d3e6d7e13800cd8af4f532ac0c0565eff640e
Divyanshu Ranjan <123241944+divyanshu-17@users.noreply.github.com>
2026-08-17 21:36:27 +0530

    Add files via upload

---
+++ FirstProject/src/Lecture 12 Collection/Set/LinkedHashSet/AddLinkedHashSet.java
@@ -0,0 +1,22 @@
+package in.Set.LinkedHashSet;
+
+import java.util.LinkedHashSet;
+
+public class AddLinkedHashSet {
+    public static void main(String[] args) {
+        LinkedHashSet<Integer> evenNumber = new LinkedHashSet<>();
+
+        // Using add() method
+        evenNumber.add(2);
+        evenNumber.add(4);
+        evenNumber.add(6);
+        System.out.println("LinkedHashSet: " + evenNumber);
+
+        LinkedHashSet<Integer> numbers = new LinkedHashSet<>();
+
+        // Using addAll() method
+        numbers.addAll(evenNumber);
+        numbers.add(5);
+        System.out.println("New LinkedHashSet: " + numbers);
+    }
+}
